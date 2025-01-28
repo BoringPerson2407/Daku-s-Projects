2583ea24669757bf9f9e5f87ea786301937ea994
Divyanshu Nautiyal <119833169+BoringPerson2407@users.noreply.github.com>
2025-01-28 08:02:13 +0530

    basic_flow of game (incomplete)

---
+++ Number_guessing_game/number_guessing_game.cpp
@@ -0,0 +1,54 @@
+// Date: 28/01/2025
+// Project: Number guessing game
+// Project function: Program will generate a random number and user has to guess it in 5 attempts
+// status: work in progress
+
+#include<iostream>
+using namespace std;
+
+bool guess(int num, int user_input, int* attempts);
+
+int main()
+{
+    int user_input, difficulty;
+    int user_guess, attempts;
+    while (user_input != -1)
+    {
+        cout << "**********************************************" << endl;
+        cout << "*                                            *" << endl;
+        cout << "*              Play the game: 0              *" << endl;
+        cout << "*              exit: -1                      *" << endl;
+        cout << "*                                            *" << endl;
+        cout << "**********************************************" << endl;
+        cin >> user_input;
+        cout << endl;
+        cout << "**********************************************" << endl;
+        cout << "*                                            *" << endl;
+        cout << "*              Difficulty: 1-3               *" << endl;
+        cout << "*                                            *" << endl;
+        cout << "**********************************************" << endl;
+        cin >> difficulty;
+        attempts = 5;
+        int num = 90;
+        while (attempts > 0)
+        {
+            cout << "Guess the number!!" << endl;
+            cin >> user_guess;
+            if (guess(num, user_guess, &attempts))
+            {   
+                cout << "You guessed the right number! ";
+                cout << "Play again: 0" << endl;
+                cout << "exit: -1" << endl;
+                cin >> user_input;
+                break;
+            }
+            
+        }
+        
+
+    }
+    
+
+
+    return 0;
+}
