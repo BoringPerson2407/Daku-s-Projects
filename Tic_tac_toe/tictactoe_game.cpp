f8f8f9a824ed50bc42155c52039bc6cc140b2446
Divyanshu Nautiyal <119833169+BoringPerson2407@users.noreply.github.com>
2025-01-30 20:43:37 +0530

    Update tictactoe_game.cpp

--- Tic_tac_toe/tictactoe_game.cpp
+++ Tic_tac_toe/tictactoe_game.cpp
@@ -1,7 +1,7 @@
 // Date: 29/01/2025
 // Project: Tic-Tac-Toe
 // Program function: A simple console based Tic-Tac-Toe game in C++
-// Status: Work in progress
+// Status: Finished
 
 #include<iostream>
 
@@ -10,7 +10,8 @@ using namespace std;
 void draw_board(char (*grid)[3]);   // this function accept grid as an input and draws a board.
 void player_move(char (*grid)[3], char player); // this function inputs the player's move and draw them on board.
 void computer_move(char (*grid)[3], char computer); // this function generates moves for computer and draw them on board.
-bool check_winner(char (*grid)[3], char  player, char computer);
+bool check_winner(char (*grid)[3], char  player, char computer); // check if winning conditions satisfied and who wins
+bool check_tie(char (*grid)[3]); // check if the game is tied
 int main()
 {
     char grid[3][3] = 
@@ -29,24 +30,27 @@ int main()
         player_move(grid, player);
         draw_board(grid);
         cout << endl;
-        if (check_winner)
+        if (check_winner(grid, player, computer))
         {
             running = false;
             break;
         }
-
-        computer_move(grid, computer);
-        draw_board(grid);
-        if (check_winner)
+        else if (check_tie(grid))
         {
             running = false;
             break;
         }
         
-        running= false;   
-    }
-    
 
+        computer_move(grid, computer);
+        draw_board(grid);
+        if (check_winner(grid, player, computer))
+        {
+            running = false;
+            break;
+        }    
+    }
+    cout << "Game over!" << endl;
     return 0;
 }
 void draw_board(char (*grid)[3])    
@@ -92,6 +96,58 @@ void computer_move(char (*grid)[3], char computer)
     }  
 }
 bool check_winner(char (*grid)[3], char  player, char computer)
+{   
+    if (grid[0][0]!='-' && grid[0][0]==grid[0][1] && grid[0][1]==grid[0][2])
+    {
+        grid[0][0] == player ? cout << "You win!\n" : cout << "You lose!\n"; 
+    }
+    else if (grid[1][0]!='-' && grid[1][0]==grid[1][1] && grid[1][1]==grid[1][2])
+    {
+        grid[1][0] == player ? cout << "You win!\n" : cout << "You lose!\n";
+    }
+    else if (grid[2][0]!='-' && grid[2][0]==grid[2][1] && grid[2][1]==grid[2][2])
+    {
+        grid[2][0] == player ? cout << "You win!\n" : cout << "You lose!\n";
+    }
+    else if (grid[0][0]!='-' && grid[0][0]==grid[1][0] && grid[1][0]==grid[2][0])
+    {
+        grid[0][0] == player ? cout << "You win!\n" : cout << "You lose!\n";
+    }
+    else if (grid[0][1]!='-' && grid[0][1]==grid[1][1] && grid[1][1]==grid[2][1])
+    {
+        grid[0][1] == player ? cout << "You win!\n" : cout << "You lose!\n";
+    }
+    else if (grid[0][2]!='-' && grid[0][2]==grid[1][2] && grid[1][2]==grid[2][2])
+    {
+        grid[0][2] == player ? cout << "You win!\n" : cout << "You lose!\n";
+    }
+    else if (grid[0][0]!='-' && grid[0][0]==grid[1][1] && grid[1][1]==grid[2][2])
+    {
+        grid[0][0] == player ? cout << "You win!\n" : cout << "You lose!\n";
+    }
+    else if (grid[0][2]!='-' && grid[0][2]==grid[1][1] && grid[1][1]==grid[2][0])
+    {
+        grid[0][2] == player ? cout << "You win!\n" : cout << "You lose!\n";
+    }
+    else
+    {
+        return false;
+    }
+    return true;
+}
+bool check_tie(char (*grid)[3])
 {
-
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            if (grid[i][j]=='-')
+            {
+                return false;
+            }
+            
+        }
+    }
+    cout << "It's a tie!\n";
+    return true;
 }
